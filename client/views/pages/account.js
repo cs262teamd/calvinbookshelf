397ed4523bd2db1d8622a8ff6a51ec8775327eb7
Arie van Luttikhuizen <aluttik@gmail.com>
2015-12-15 16:48:19 -0500

    Documented javascript files

--- client/views/pages/account.js
+++ client/views/pages/account.js
@@ -1,11 +1,14 @@
+/////////////////////////////// account template //////////////////////////////
+
 // binds to the account template when its created
-Template.account.onCreated(function(){
+Template.account.onCreated(function() {
     this.subscribe("books");
 });
 
 // helper functions bound to the account template
 Template.account.helpers({
-    books: function() {
+    // returns all of the books that the user is selling
+    myBooks: function() {
         return Books.find({user: Meteor.user().username});
     }
 });
--- client/views/pages/faq.js
+++ client/views/pages/faq.js
@@ -1,6 +1,9 @@
-// helper function bound to the faq template
+///////////////////////////////// faq template ////////////////////////////////
+
+// onRendered hook bound to the faq template
 Template.faq.onRendered(function() {
+    // initializes elements with the .collapsible class
     $('.collapsible').collapsible({
-        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        accordion : false
     });
 });
